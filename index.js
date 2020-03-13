b897b9259186c0e463f71f24a8580b681b04b842
Evrgreen <rcarsten06@gmail.com>
2020-03-13 11:21:35 -0600

    tested endpoints, project complete

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const server = require("./data/api/server.js");
 port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(
-    `\n* ${Date(
+    `\n ${Date(
       Date.now
-    ).toString()}Server listening on https://localhost:${port} *\n`
+    ).toString()}\n Server listening on https://localhost:${port} \n`
   );
 });
